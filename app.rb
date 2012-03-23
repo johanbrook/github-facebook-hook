6eefb1bc29c8bb995fd846f0addd24d54593f5fa
Johan Brook <johan.jvb@gmail.com>
2012-03-23 16:12:49 +0100

    Only post pushes made to master branch

--- app.rb
+++ app.rb
@@ -28,9 +28,11 @@ post "/facebook" do
     push["head_commit"]["modified"].length, 
     push["head_commit"]["added"].length, 
     push["head_commit"]["removed"].length
+  
+  ref = push['ref'].split("/").last
     
   post = {
-    :link => push["repository"]["url"]+"/commits/"+push['ref'].split("/").last,
+    :link => push["repository"]["url"]+"/commits/"+ref,
     :name => sprintf("HEAD is now %s", push["after"][0..7]),
     :description => push["head_commit"]["message"],
     :caption => caption
@@ -38,9 +40,9 @@ post "/facebook" do
   
   msg = sprintf("%s pushed new stuff to %s (%s commit(s))",
           push["pusher"]["name"].split.first, 
-          push["ref"].split("/").last,
+          ref,
           push['commits'].length)
           
-  ret = graph.put_wall_post(msg, post, GROUP_ID)
-  puts ret["id"]
+    # Only post pushes made to master
+    graph.put_wall_post(msg, post, GROUP_ID) if ref == "master"
 end
